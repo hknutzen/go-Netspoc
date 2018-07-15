3212a3f90ac6d1e9d8b35c6624a47e0cbf298bb7
Heinz Knutzen <heinz.knutzen@gmail.com>
2018-07-15 21:39:59 +0200

    Fehlerfreier Zwischenstand

--- main.go
+++ main.go
@@ -1,6 +1,6 @@
 package main
 import (
-//	"fmt"
+	"fmt"
 	"io/ioutil"
 //	"github.com/davecgh/go-spew/spew"
 	"github.com/Sereal/Sereal/Go/sereal"
@@ -13,7 +13,11 @@ type xAny interface{}
 type xMap = map[string]interface{}
 type xArray = []interface{}
 
-type netObj interface{}
+type someObj interface{
+	up () someObj
+	setCommon (m xMap)
+}
+type pathObj interface {}
 
 func convertBool (x xAny) bool {
 	switch x.(type) {
@@ -58,18 +62,32 @@ func getMap (x xAny) xMap {
 	}
 }
 
-type Network struct {
+type IPObj struct {
 	Name string
 	IP net.IP
+	Up someObj
+}
+
+func (x *IPObj) up () someObj { return x.Up }
+func (x *IPObj) setCommon (m xMap) {
+	x.Name = m["name"].(string)
+	ip := m["ip"]
+	if ip == nil {
+		fmt.Println(x.Name)
+	} else {
+		x.IP = m["ip"].([]byte)
+	}
+	if up, ok := m["up"]; ok {
+		x.Up = convertNetObj(up)
+	}
+}
+
+type Network struct {
+	IPObj
 	Mask net.IPMask
 	Subnets []*Subnet
 	Interfaces []*Interface
-	Up netObj
 }
-
-func (x *Network)   up () netObj { return x.up }
-func (x *Subnet)    up () netObj { return x.up }
-func (x *Interface) up () netObj { return x.up }
 
 func convertNetwork (x xAny) *Network {
 	m := getMap(x) 
@@ -78,9 +96,10 @@ func convertNetwork (x xAny) *Network {
 	}
 	n := new(Network)
 	m["ref"] = n
-	n.Name = m["name"].(string)
-	n.IP = m["ip"].([]byte)
-	n.Mask = m["mask"].([]byte)
+	n.setCommon(m)
+	if m["mask"] != nil {
+		n.Mask = m["mask"].([]byte)
+	}
 	if list, ok := m["subnets"]; ok {
 		xSubnets := list.(xArray)
 		subnets := make([]*Subnet, len(xSubnets))
@@ -97,32 +116,25 @@ func convertNetwork (x xAny) *Network {
 		}
 		n.Interfaces = interfaces
 	}
-	if up, ok := m["up"]; ok {
-		n.Up = convertNetObj(up)
-	}
 	return n
 }
 
 type Subnet struct {
-	Name string
-	IP net.IP
+	IPObj
+	Mask net.IPMask
 	Network *Network
-	Up netObj
 }
 func convertSubnet (x xAny) *Subnet {
 	m := getMap(x) 
-	if h, ok := m["ref"]; ok {
-		return h.(*Subnet)
+	if s, ok := m["ref"]; ok {
+		return s.(*Subnet)
 	}
-	h := new(Subnet)
-	m["ref"] = h
-	h.Name = m["name"].(string)
-	h.IP = m["ip"].([]byte)
-	h.Network = convertNetwork(m["network"])
-	if up, ok := m["up"]; ok {
-		h.Up = convertNetObj(up)
-	}
-	return h
+	s := new(Subnet)
+	m["ref"] = s
+	s.setCommon(m)
+	s.Mask = m["mask"].([]byte)
+	s.Network = convertNetwork(m["network"])
+	return s
 }
 type Router struct {
 	Name string
@@ -149,11 +161,9 @@ func convertRouter (x xAny) *Router {
 }
 
 type Interface struct {
-	Name string
-	IP net.IP
+	IPObj
 	Network *Network
 	Router *Router
-	Up netObj
 }
 func convertInterface (x xAny) *Interface {
 	m := getMap(x) 
@@ -162,19 +172,15 @@ func convertInterface (x xAny) *Interface {
 	}
 	i := new(Interface)
 	m["ref"] = i
-	i.Name = m["name"].(string)
-	i.IP = m["ip"].([]byte)
+	i.setCommon(m)
 	i.Network = convertNetwork(m["network"])
-	if up, ok := m["up"]; ok {
-		i.Up = convertNetObj(up)
-	}
 	return i
 }
 
-func convertNetObj (x xAny) netObj {
+func convertNetObj (x xAny) someObj {
 	m := getMap(x) 
 	if o, ok := m["ref"]; ok {
-		return o
+		return o.(someObj)
 	}
 	if _, ok := m["router"]; ok {
 		return convertInterface(x)
@@ -185,25 +191,62 @@ func convertNetObj (x xAny) netObj {
 	return convertNetwork(x)
 }
 
-func convertNetObjects (x xAny) []netObj {
+func convertNetObjects (x xAny) []someObj {
 	a := getArray(x)
-	objects := make([]netObj, len(a))
+	objects := make([]someObj, len(a))
 	for i, x := range a {
 		objects[i] = convertNetObj(x)
 	}
 	return objects
 }
 
+type Zone struct {
+	Name string
+	Networks []*Network
+}
+func convertZone (x xAny) *Zone {
+	m := getMap(x) 
+	if r, ok := m["ref"]; ok {
+		return r.(*Zone)
+	}
+	z := new(Zone)
+	m["ref"] = z
+	z.Name = m["name"].(string)
+	if list, ok := m["networks"]; ok {
+		xNetworks := list.(xArray)
+		networks := make([]*Network, len(xNetworks))
+		for i, xNetwork := range xNetworks {
+			networks[i] = convertNetwork(xNetwork)
+		}
+		z.Networks = networks
+	}
+	return z
+}
+
+func convertPathObj (x xAny) pathObj {
+	m := getMap(x) 
+	if o, ok := m["ref"]; ok {
+		return o.(pathObj)
+	}
+	if _, ok := m["router"]; ok {
+		return convertInterface(x)
+	}
+	if _, ok := m["managed"]; ok {
+		return convertRouter(x)
+	}
+	return convertZone(x)
+}
+
 type Rule struct {
 	Deny bool
-	Src []netObj
-	Dst []netObj
+	Src []someObj
+	Dst []someObj
 	//Prt []Prt
 	//SrcRange Prt
 	// Rule xAny
 	Log []string
-	//SrcPath pathObj
-	//DstPath pathObj
+	SrcPath pathObj
+	DstPath pathObj
 	Stateless bool
 	StatelessICMP bool
 }
@@ -212,6 +255,8 @@ func convertRule (m xMap) *Rule {
 	r.Deny = convertBool(m["deny"])
 	r.Src = convertNetObjects(m["src"])
 	r.Dst = convertNetObjects(m["dst"])
+	r.SrcPath = convertPathObj(m["src_path"])
+	r.DstPath = convertPathObj(m["dst_path"])
 	if list, ok := m["log"]; ok {
 		r.Log = convertStrings(list)
 	}
@@ -243,6 +288,67 @@ func convertPathRules (m xMap) *PathRules {
 	return rules
 }
 
+func check_expanded_rules (pRules PathRules) {
+	//var count int
+
+	// Process rules in chunks to reduce memory usage.
+	// Rules with different src_path / dst_path can't be
+   // redundant to each other.
+   // Keep deterministic order of rules.
+	var index = 0
+	path2index := make(map[pathObj]int)
+	key2rules := make(map[int][]*Rule)
+	add := func (rules []*Rule) {
+		for _, rule := range rules {
+			key, ok := path2index[rule.SrcPath] 
+			if !ok {
+				key = index
+				index++
+				path2index[rule.SrcPath] = key
+			}
+			key2rules[key] = append(key2rules[key], rule)
+		}
+	}
+	add(pRules.Deny)
+	add(pRules.Permit)
+
+	for key := 0; key < index; key++ {
+		rules := key2rules[key]
+		var index = 0
+		path2index := make(map[pathObj]int)
+		key2rules := make(map[int][]*Rule)
+		for _, rule := range rules {
+			key, ok := path2index[rule.DstPath] 
+			if !ok {
+				key = index
+				index++
+				path2index[rule.DstPath] = key
+			}
+			key2rules[key] = append(key2rules[key], rule)
+		}
+		for key := 0; key < index; key++ {
+			_ = key2rules[key]
+			//rules := key2rules[key]
+			//expandedRules := expandRules(rules)
+			//count += len(expandedRules)
+			//ruleTree, deleted := buildRuleTree(expanded_rules)
+			//dcount += deleted
+			//setLocalPrtRelation(rules);
+			//rcount += findRedundantRules(rule_tree, rule_tree);
+		}
+	}
+	/*
+	show_duplicate_rules()
+	show_redundant_rules()
+	warn_unused_overlaps()
+	show_fully_redundant_rules()
+	info("Expanded rule count: $count; duplicate: $dcount; redundant: $rcount");
+   */
+}
+
+		
+		
+		
 func main() {
 	bytes, err := ioutil.ReadFile(path)
 	if err != nil {
